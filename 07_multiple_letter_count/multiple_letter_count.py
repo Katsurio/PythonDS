c0d6b0252ee10cea97e7758d7f869ffc9b188509
John E. Fisher (Jef) <fishe545@gmail.com>
2021-08-15 09:30:21 +0800

    feat: 07 finished

--- 07_multiple_letter_count/multiple_letter_count.py
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -7,3 +7,11 @@ def multiple_letter_count(phrase):
         >>> multiple_letter_count('Yay')
         {'Y': 1, 'a': 1, 'y': 1}
     """
+    freq = {}
+    count = 0
+    for char in phrase:
+        if freq.get(char):
+            freq[char] += 1
+        else: 
+            freq[char] = 1
+    return freq
