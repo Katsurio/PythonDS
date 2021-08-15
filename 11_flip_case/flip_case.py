568fbb8a559f935fbdb62f4e6f9cfb8c604567ba
John E. Fisher (Jef) <fishe545@gmail.com>
2021-08-15 14:42:11 +0800

    feat: 11 finished

--- 11_flip_case/flip_case.py
+++ 11_flip_case/flip_case.py
@@ -11,3 +11,24 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    to_swap = to_swap.lower()
+    newStr = ''
+    
+    for char in phrase:
+        if char.lower() == to_swap:
+            char = char.swapcase()
+        newStr += char
+    return newStr
+
+
+# Alternate phrasing: a bit clever, same runtime, and harder to
+    # read:
+
+    # to_swap = to_swap.lower()
+    #
+    # fixed = [
+    #     (char.swapcase() if char.lower() == to_swap else char)
+    #     for char in phrase
+    # ]
+    #
+    # return "".join(fixed)
