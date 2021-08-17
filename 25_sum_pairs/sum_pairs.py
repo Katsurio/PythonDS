ca15a7b4a006d5e2265cc2e32e01361b51cb9f2e
John E. Fisher (Jef) <fishe545@gmail.com>
2021-08-17 15:21:25 +0800

    wip: working on problem 25: sum_pairs

--- 25_sum_pairs/sum_pairs.py
+++ 25_sum_pairs/sum_pairs.py
@@ -21,3 +21,28 @@ def sum_pairs(nums, goal):
         >>> sum_pairs([11, 20, 4, 2, 1, 5], 100)
         ()
     """
+    first_match = None
+    second_match = None
+    for num in nums:
+        if second_match:
+            break   
+        for n in nums:    
+            if num + n == goal:
+                if second_match:
+                    break
+                if first_match == None:
+                    first_match = (num, n) 
+                elif (second_match == None and first_match[0] != num):
+                    second_match = (num, n)
+    
+    if second_match != None:
+        # print("IF second_match if hit")
+        # return first_match if nums.index(first_match[0]) < nums.index(second_match[0]) and nums.index(first_match[1]) < nums.index(second_match[1]) else second_match
+        return first_match if nums.index(first_match[1]) < nums.index(second_match[1]) else second_match
+    elif first_match != None:
+        # print("ELIF first _match if hit")
+        return first_match
+    else:
+        # print("ELSE hit")
+        return ()
+# sum_pairs([5, 1, 4, 8, 3, 2], 7)
