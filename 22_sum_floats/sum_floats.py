a5b6db6248b2d2ec1121c8cdb7a908c71013454f
John E. Fisher (Jef) <fishe545@gmail.com>
2021-08-17 08:16:13 +0800

    feat: 22 finished

--- 22_sum_floats/sum_floats.py
+++ 22_sum_floats/sum_floats.py
@@ -11,3 +11,10 @@ def sum_floats(nums):
     # hint: to find out if something is a float, you should use the
     # "isinstance" function --- research how to use this to find out
     # if something is a float!
+    output = 0
+    
+    for num in nums:
+        if isinstance(num, float):
+            output += num
+    # print([num for num in nums if isinstance(num,float)])
+    return output
