0e2a177e5bc025f4cc2b639fd5e72312dcd716bb
John E. Fisher (Jef) <fishe545@gmail.com>
2021-08-18 15:19:34 +0800

    feat: fs_2 is finished

--- fs_2_valid_parentheses/valid_parentheses.py
+++ fs_2_valid_parentheses/valid_parentheses.py
@@ -22,3 +22,18 @@ def valid_parentheses(parens):
         >>> valid_parentheses(")()(")
         False
     """
+    length = len(parens)
+    
+    left = '('
+    l_count = 0
+    right = ')'
+    r_count = 0
+    
+    if parens[0] == right: return False
+    for p in parens:
+        if p == left:
+            l_count += 1
+        elif p == right:
+            r_count += 1
+            
+    return True if l_count == r_count else False
